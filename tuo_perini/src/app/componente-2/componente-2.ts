aa75343afac155c5db7b06f79d1547f1b32743e9
Giorgio-Perini <perini.giorgio@iisgalvanimi.edu.it>
2025-10-02 08:33:07 +0000

    commit 2

---
+++ tuo_perini/src/app/componente-2/componente-2.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-componente-2',
+  imports: [],
+  templateUrl: './componente-2.html',
+  styleUrl: './componente-2.css'
+})
+export class Componente2 {
+
+}
---
+++ tuo_perini/src/app/componente-3/componente-3.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-componente-3',
+  imports: [],
+  templateUrl: './componente-3.html',
+  styleUrl: './componente-3.css'
+})
+export class Componente3 {
+
+}
